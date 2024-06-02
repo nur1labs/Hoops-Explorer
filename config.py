d40e8443d945e67c3ed18d9c65bf2e4fb71e05eb
root <crbru.yuthafiga@gmail.com>
2024-06-02 18:20:58 +0700

    [db]fixed all

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 rid = "rci-explorer"
+#endpoint = "http://sora:t3st1234@localhost:40131/"
 #RPC Auth(not support) endpoint = "http://sora:be9501aaa4e95e09b447ea36f009661e$cc77e8c8b0debe3dcc23e7d40fe957ddfc6c5bd5140f378b583d3a872928cde0@localhost:41131/"
 endpoint = "http://92.205.236.136:3031/"
 secret = "im love charlotte"
