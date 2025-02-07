9aa59388c5f88b81e25b6e870b2c1ab90173fd39
root <crbru.yuthafiga@gmail.com>
2025-02-07 15:02:57 +0700

    [web]hoops data explorer

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-rid = "rci-explorer"
+rid = "hoops-explorer"
 #endpoint = "http://sora:t3st1234@localhost:40131/"
 #RPC Auth(not support) endpoint = "http://sora:be9501aaa4e95e09b447ea36f009661e$cc77e8c8b0debe3dcc23e7d40fe957ddfc6c5bd5140f378b583d3a872928cde0@localhost:41131/"
-endpoint = "http://92.205.236.136:3031/"
+endpoint = "http://139.59.168.91:3031/"
 secret = "im love charlotte"
 #host = "http://20.243.210.93/"
-host = "92.205.236.136"
+host = "139.59.168.91"
 debug = True
 port = 4321
 
 db_params = {
     "provider": "sqlite",
-    "filename": "/home/soradev/minetok_explorer/mtk.db",
+    "filename": "/root/hoops_explorer/hoops.db",
     "create_db": True
 }
