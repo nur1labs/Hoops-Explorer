2e4768ca222b0b572a4a7dc832316177aab96a54
root <crbru.yuthafiga@gmail.com>
2025-02-07 15:09:41 +0700

    [web]hoops data explorer

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ port = 4321
 
 db_params = {
     "provider": "sqlite",
-    "filename": "/root/coins/hoops_explorer/hoops.db",
+    "filename": "/root/coins/Hoops-Explorer/hoops.db",
     "create_db": True
 }
